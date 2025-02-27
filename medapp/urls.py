24c7b3d545697e6cc471c12e3df8a4393a80520e
Nowellshiro <noellshy@gmail.com>
2025-02-27 08:26:26 +0300

    This is my tenth commit

--- medapp/urls.py
+++ medapp/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('starter/', views.starter, name='starter'),
-
+    path('about/', views.about, name='about'),
+    path('service/', views.service, name='service'),
+    path('department/', views.department, name='department'),
+    path('doctor/', views.doctor, name='doctor'),
 ]
 
